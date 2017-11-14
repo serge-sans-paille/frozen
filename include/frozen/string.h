74c7121a4593cc6c16e990652bee45405bd9e505
Chris Beck <chbeck@tesla.com>
2017-11-13 16:41:18 -0800

    test for frozen map<string>, make string comparable

--- include/frozen/string.h
+++ include/frozen/string.h
@@ -33,14 +33,17 @@
 namespace frozen {
 
 class string {
-  char const *const data_;
-  std::size_t const size_;
+  char const * data_;
+  std::size_t size_;
 
   public:
   constexpr string(char const *data, std::size_t size)
       : data_(data), size_(size) {}
 
   string(std::string const &s) : data_(s.data()), size_(s.size()) {}
+
+  constexpr string(const string &) noexcept = default;
+  constexpr string & operator=(const string &) noexcept = default;
 
   constexpr std::size_t size() const { return size_; }
 
@@ -53,6 +56,16 @@ class string {
       if (data_[i] != other.data_[i])
         return false;
     return true;
+  }
+
+  constexpr bool operator < (const string & other) const {
+    unsigned i = 0;
+    while (i < size() && i < other.size()) {
+      if ((*this)[i] < other[i]) { return true; }
+      if ((*this)[i] > other[i]) { return false; }
+      ++i;
+    }
+    return size() < other.size();
   }
 };
 
