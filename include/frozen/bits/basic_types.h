89781408fff1c2a4ea33f7a078f233f8aef80adf
Jerome Dumesnil <jerome.dumesnil@gmail.com>
2017-12-23 10:24:16 +0100

    Introduce constexpr-array

--- include/frozen/bits/basic_types.h
+++ include/frozen/bits/basic_types.h
@@ -24,6 +24,7 @@
 #define FROZEN_LETITGO_BASIC_TYPES_H
 
 #include <array>
+#include <stdexcept>
 
 namespace frozen {
 
@@ -86,6 +87,67 @@ public:
   }
 };
 
+template <class T, std::size_t N>
+class carray {
+  T data_ [N] = {}; // zero-initialization for scalar type T, default-initialized otherwise
+
+public:
+  // Container typdefs
+  using value_type = T;
+  using reference = value_type &;
+  using const_reference = const value_type &;
+  using pointer = value_type *;
+  using const_pointer = const value_type *;
+  using iterator = pointer;
+  using const_iterator = const_pointer;
+  using size_type = std::size_t;
+  using difference_type = std::ptrdiff_t;
+
+  // Constructors
+  // constexpr carray(void) = default;
+  // constexpr carray(size_type count, const T& value) : dsize(count) {
+  //   for (std::size_t i = 0; i < N; ++i)
+  //     data_[i] = value;
+  // }
+
+  // Iterators
+  constexpr iterator begin() noexcept { return data_; }
+  constexpr iterator end() noexcept { return data_ + N; }
+
+  // Capacity
+  constexpr size_type size() const { return N; }
+  constexpr size_type max_size() const { return N; }
+
+  // Element access
+  constexpr       reference operator[](std::size_t index) { return data_[index]; }
+  constexpr const_reference operator[](std::size_t index) const { return data_[index]; }
+
+  constexpr       reference at(std::size_t index) {
+    if (index > N)
+      throw std::out_of_range("Index (" + std::to_string(index) + ") out of bound (" + std::to_string(N) + ')');
+    return data_[index];
+  }
+  constexpr const_reference at(std::size_t index) const {
+    if (index > N)
+      throw std::out_of_range("Index (" + std::to_string(index) + ") out of bound (" + std::to_string(N) + ')');
+    return data_[index];
+  }
+
+  constexpr       reference front() { return data_[0]; }
+  constexpr const_reference front() const { return data_[0]; }
+
+  constexpr       reference back() { return data_[N - 1]; }
+  constexpr const_reference back() const { return data_[N - 1]; }
+
+  constexpr       value_type* data() noexcept { return data_; }
+  constexpr const value_type* data() const noexcept { return data_; }
+
+  // Modifiers
+  constexpr void fill(const value_type& val) {
+    for (std::size_t i = 0; i < N; ++i)
+      data_[i] = val;
+  }
+};
 
 } // namespace bits
 
