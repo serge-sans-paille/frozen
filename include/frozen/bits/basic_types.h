cb684f6146b2b774d9511f79c9ddb167da571415
serge-sans-paille <sguelton@quarkslab.com>
2018-08-20 16:07:39 +0200

    Specialize carray for 0 elements
    Avoids invalid use of 0-size plain array.

--- include/frozen/bits/basic_types.h
+++ include/frozen/bits/basic_types.h
@@ -168,6 +168,27 @@ public:
       data_[i] = val;
   }
 };
+template <class T>
+class carray<T, 0> {
+
+public:
+  // Container typdefs
+  using value_type = T;
+  using reference = value_type &;
+  using const_reference = const value_type &;
+  using pointer = value_type *;
+  using const_pointer = const value_type *;
+  using iterator = pointer;
+  using const_iterator = const_pointer;
+  using reverse_iterator = std::reverse_iterator<iterator>;
+  using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+  using size_type = std::size_t;
+  using difference_type = std::ptrdiff_t;
+
+  // Constructors
+  constexpr carray(void) = default;
+
+};
 
 } // namespace bits
 
