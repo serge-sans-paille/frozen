ea17cde4493b28470c794210751d47b017da9de1
Peter Moran <petecmo@gmail.com>
2020-12-07 14:34:20 -0500

    #105 Add non-const functions to map and unordered_map that allow value modification (#106)
    * Complete implementation of non-const functions for map and unordered_map
    * Add unit tests
    * Update README to explain non-constexpr maps

---
+++ examples/value_modification.cpp
@@ -0,0 +1,37 @@
+#include <frozen/set.h>
+#include <frozen/string.h>
+#include <frozen/unordered_map.h>
+#include <iostream>
+
+/// MAYBE_CONSTINIT expands to `constinit` if available.
+#if __cpp_constinit
+#define MAYBE_CONSTINIT constinit
+#else
+#define MAYBE_CONSTINIT
+#endif
+
+// To make a frozen::unordered_map where you can modify the values, make a
+// non-constexpr instance. If available, prefer to use constinit. It will
+// initialize the map during compilation and detect any exceptions.
+MAYBE_CONSTINIT static frozen::unordered_map<frozen::string, int, 2> fruits = {
+    {"n_apples", 0},
+    {"n_pears", 0},
+};
+
+int main() {
+  // Update the values using at()
+  fruits.at("n_apples") = 10;
+  fruits.at("n_pears") = fruits.at("n_apples") * 2;
+  std::cout << "n_apples: " << fruits.at("n_apples") << std::endl;
+  std::cout << "n_pears: " << fruits.at("n_pears") << std::endl;
+
+  // You can also update values via the iterator returned by find()
+  auto found = fruits.find("n_apples");
+  found->second = 0;
+  std::cout << "n_apples: " << fruits.at("n_apples") << std::endl;
+
+  // Range also works
+  auto range = fruits.equal_range("n_apples");
+  range.first->second = 1337;
+  std::cout << "n_apples: " << fruits.at("n_apples") << std::endl;
+}
--- include/frozen/map.h
+++ include/frozen/map.h
@@ -82,15 +82,15 @@ public:
   using size_type = typename container_type::size_type;
   using difference_type = typename container_type::difference_type;
   using key_compare = decltype(less_than_);
+  using reference = typename container_type::reference;
   using const_reference = typename container_type::const_reference;
-  using reference = const_reference;
+  using pointer = typename container_type::pointer;
   using const_pointer = typename container_type::const_pointer;
-  using pointer = const_pointer;
+  using iterator = typename container_type::iterator;
   using const_iterator = typename container_type::const_iterator;
-  using iterator = const_iterator;
+  using reverse_iterator = typename container_type::reverse_iterator;
   using const_reverse_iterator =
       typename container_type::const_reverse_iterator;
-  using reverse_iterator = const_reverse_iterator;
 
 public:
   /* constructors */
@@ -110,22 +110,25 @@ public:
       : map{items, Compare{}} {}
 
   /* element access */
-  constexpr mapped_type at(Key const &key) const {
-    auto const where = lower_bound(key);
-    if (where != end())
-      return where->second;
-    else
-      FROZEN_THROW_OR_ABORT(std::out_of_range("invalid key"));
+  constexpr Value& at(Key const &key) const {
+    return at_impl(*this, key);
+  }
+  constexpr Value& at(Key const &key) {
+    return at_impl(*this, key);
   }
 
   /* iterators */
+  constexpr iterator begin() { return items_.begin(); }
   constexpr const_iterator begin() const { return items_.begin(); }
   constexpr const_iterator cbegin() const { return items_.cbegin(); }
+  constexpr iterator end() { return items_.end(); }
   constexpr const_iterator end() const { return items_.end(); }
   constexpr const_iterator cend() const { return items_.cend(); }
 
+  constexpr reverse_iterator rbegin() { return items_.rbegin(); }
   constexpr const_reverse_iterator rbegin() const { return items_.rbegin(); }
   constexpr const_reverse_iterator crbegin() const { return items_.crbegin(); }
+  constexpr reverse_iterator rend() { return items_.rend(); }
   constexpr const_reverse_iterator rend() const { return items_.rend(); }
   constexpr const_reverse_iterator crend() const { return items_.crend(); }
 
@@ -141,40 +144,84 @@ public:
   }
 
   constexpr const_iterator find(Key const &key) const {
-    const_iterator where = lower_bound(key);
-    if ((where != end()) && !less_than_(key, *where))
-      return where;
-    else
-      return end();
+    return find_impl(*this, key);
+  }
+  constexpr iterator find(Key const &key) {
+    return find_impl(*this, key);
   }
 
-  constexpr std::pair<const_iterator, const_iterator> equal_range(Key const &key) const {
-    auto const lower = lower_bound(key);
-    if (lower == end())
-      return {lower, lower};
-    else
-      return {lower, lower + 1};
+  constexpr std::pair<const_iterator, const_iterator>
+  equal_range(Key const &key) const {
+    return equal_range_impl(*this, key);
+  }
+  constexpr std::pair<iterator, iterator> equal_range(Key const &key) {
+    return equal_range_impl(*this, key);
   }
 
   constexpr const_iterator lower_bound(Key const &key) const {
-    auto const where = bits::lower_bound<N>(items_.begin(), key, less_than_);
-    if ((where != end()) && !less_than_(key, *where))
-      return where;
-    else
-      return end();
+    return lower_bound_impl(*this, key);
+  }
+  constexpr iterator lower_bound(Key const &key) {
+    return lower_bound_impl(*this, key);
   }
 
   constexpr const_iterator upper_bound(Key const &key) const {
-    auto const where = bits::lower_bound<N>(items_.begin(), key, less_than_);
-    if ((where != end()) && !less_than_(key, *where))
-      return where + 1;
-    else
-      return end();
+    return upper_bound_impl(*this, key);
+  }
+  constexpr iterator upper_bound(Key const &key) {
+    return upper_bound_impl(*this, key);
   }
 
   /* observers */
   constexpr key_compare key_comp() const { return less_than_; }
   constexpr key_compare value_comp() const { return less_than_; }
+
+ private:
+  template <class This>
+  static inline constexpr auto& at_impl(This&& self, Key const &key) {
+    auto where = self.lower_bound(key);
+    if (where != self.end())
+      return where->second;
+    else
+      FROZEN_THROW_OR_ABORT(std::out_of_range("unknown key"));
+  }
+
+  template <class This>
+  static inline constexpr auto find_impl(This&& self, Key const &key) {
+    auto where = self.lower_bound(key);
+    if ((where != self.end()) && !self.less_than_(key, *where))
+      return where;
+    else
+      return self.end();
+  }
+
+  template <class This>
+  static inline constexpr auto equal_range_impl(This&& self, Key const &key) {
+    auto lower = self.lower_bound(key);
+    using lower_t = decltype(lower);
+    if (lower == self.end())
+      return std::pair<lower_t, lower_t>{lower, lower};
+    else
+      return std::pair<lower_t, lower_t>{lower, lower + 1};
+  }
+
+  template <class This>
+  static inline constexpr auto lower_bound_impl(This&& self, Key const &key) -> decltype(self.end()) {
+    auto where = bits::lower_bound<N>(self.items_.begin(), key, self.less_than_);
+    if ((where != self.end()) && !self.less_than_(key, *where))
+      return where;
+    else
+      return self.end();
+  }
+
+  template <class This>
+  static inline constexpr auto upper_bound_impl(This&& self, Key const &key) -> decltype(self.end()) {
+    auto where = bits::lower_bound<N>(self.items_.begin(), key, self.less_than_);
+    if ((where != self.end()) && !self.less_than_(key, *where))
+      return where + 1;
+    else
+      return self.end();
+  }
 };
 
 template <class Key, class Value, class Compare>
@@ -189,14 +236,14 @@ public:
   using size_type = typename container_type::size_type;
   using difference_type = typename container_type::difference_type;
   using key_compare = decltype(less_than_);
+  using reference = typename container_type::reference;
   using const_reference = typename container_type::const_reference;
-  using reference = const_reference;
+  using pointer = typename container_type::pointer;
   using const_pointer = typename container_type::const_pointer;
-  using pointer = const_pointer;
-  using const_iterator = pointer;
   using iterator = pointer;
-  using const_reverse_iterator = pointer;
+  using const_iterator = const_pointer;
   using reverse_iterator = pointer;
+  using const_reverse_iterator = const_pointer;
 
 public:
   /* constructors */
@@ -210,15 +257,22 @@ public:
   constexpr mapped_type at(Key const &) const {
     FROZEN_THROW_OR_ABORT(std::out_of_range("invalid key"));
   }
+  constexpr mapped_type at(Key const &) {
+    FROZEN_THROW_OR_ABORT(std::out_of_range("invalid key"));
+  }
 
   /* iterators */
+  constexpr iterator begin() { return nullptr; }
   constexpr const_iterator begin() const { return nullptr; }
   constexpr const_iterator cbegin() const { return nullptr; }
+  constexpr iterator end() { return nullptr; }
   constexpr const_iterator end() const { return nullptr; }
   constexpr const_iterator cend() const { return nullptr; }
 
+  constexpr reverse_iterator rbegin() { return nullptr; }
   constexpr const_reverse_iterator rbegin() const { return nullptr; }
   constexpr const_reverse_iterator crbegin() const { return nullptr; }
+  constexpr reverse_iterator rend() { return nullptr; }
   constexpr const_reverse_iterator rend() const { return nullptr; }
   constexpr const_reverse_iterator crend() const { return nullptr; }
 
@@ -232,15 +286,22 @@ public:
   constexpr std::size_t count(Key const &) const { return 0; }
 
   constexpr const_iterator find(Key const &) const { return end(); }
+  constexpr iterator find(Key const &) { return end(); }
 
   constexpr std::pair<const_iterator, const_iterator>
   equal_range(Key const &) const {
     return {end(), end()};
   }
+  constexpr std::pair<iterator, iterator>
+  equal_range(Key const &) {
+    return {end(), end()};
+  }
 
   constexpr const_iterator lower_bound(Key const &) const { return end(); }
+  constexpr iterator lower_bound(Key const &) { return end(); }
 
   constexpr const_iterator upper_bound(Key const &) const { return end(); }
+  constexpr iterator upper_bound(Key const &) { return end(); }
 
   /* observers */
   constexpr key_compare key_comp() const { return less_than_; }
